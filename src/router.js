8b862bb5931e3bc4bf4312c87dda703cf76724d4
KaushikDuddala <kaushik.duddala@gmail.com>
2024-06-27 16:46:06 -0500

    implemented other functions into router

--- src/router.js
+++ src/router.js
@@ -3,6 +3,10 @@ import { Router } from 'itty-router';
 let getSession, getSchedule, getStudentInfo, getClasses, getStudentGpa;
 async function loadModules() {
   await import("../FISDHac/login.mjs").then(x => getSession = x.getSession)
+  //await import("../FISDHac/classes.mjs").then(x => getClasses = x.getClasses)
+  await import("../FISDHac/schedule.mjs").then(x => getSchedule = x.getSchedule)
+  await import("../FISDHac/studentinfo.mjs").then(x => getStudentInfo = x.getStudentInfo)
+  await import("../FISDHac/gpa.mjs").then(x => getStudentGpa = x.getStudentGpa)
 }
 
 
@@ -15,6 +19,34 @@ router.get('/login', async (request) => {
     const token = await getSession(user, pass);
     return new Response(token);
 });
+
+router.get('/schedule', async (request) => {
+    const { user, pass } = request.query;
+    const token = await getSession(user, pass);
+    const schedule = await getSchedule(token);
+    return new Response(schedule);
+});
+
+router.get('/studentinfo', async (request) => {
+    const { user, pass } = request.query;
+    const token = await getSession(user, pass);
+    const studentInfo = await getStudentInfo(token);
+    return new Response(studentInfo);
+});
+/*
+router.get('/classes', async (request) => {
+    const { user, pass } = request.query;
+    const token = await getSession(user, pass);
+    const classes = await getClasses(token);
+    return new Response(classes);
+});*/
+
+router.get('/gpa', async (request) => {
+    const { user, pass } = request.query
+    const token = await getSession(user, pass)
+    const gpa = await getStudentGpa(token)
+    return new Response(gpa)
+})
 
 router.all('*', () => new Response('Not Found.', { status: 404 }));
 
