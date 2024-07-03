8330e40b09bacaa7ff0131a821de8244cca05082
KaushikDuddala <kaushik.duddala@gmail.com>
2024-07-03 15:03:03 -0500

    introduced all endpoint and format validation

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { Router } from 'itty-router';
 let getSession, getSchedule, getStudentInfo, getClasses, getStudentGpa;
 async function loadModules() {
   await import("../FISDHac/login.mjs").then(x => getSession = x.getSession)
-  //await import("../FISDHac/classes.mjs").then(x => getClasses = x.getClasses)
+  await import("../FISDHac/classes.mjs").then(x => getClasses = x.getClasses)
   await import("../FISDHac/schedule.mjs").then(x => getSchedule = x.getSchedule)
   await import("../FISDHac/studentinfo.mjs").then(x => getStudentInfo = x.getStudentInfo)
   await import("../FISDHac/gpa.mjs").then(x => getStudentGpa = x.getStudentGpa)
@@ -12,7 +12,21 @@ async function loadModules() {
 
 const router = Router();
 
-//router.get('/api', () => new Response('Todos Index!'));
+async function validateUserAndPass(reqBody) {
+    let user
+    let pass
+    try {
+      user = reqBody.username
+      pass = reqBody.password
+    } catch (error) {
+      return [400, error]
+    }
+    if (!/[0-9][0-9][0-9][0-9][0-9][0-9]/.test(user)) {
+      return [400, "username does not match XXXXXX"]
+    }
+    return [user, pass]
+  }
+
 
 router.get('/login', async (request) => {
     const { user, pass } = request.query;
@@ -21,6 +35,8 @@ router.get('/login', async (request) => {
 });
 
 router.get('/schedule', async (request) => {
+    const checkPass = await validateUserAndPass(request.query)
+    if(!checkPass) return new Response("Invalid username or password", { status: 400 })
     const { user, pass } = request.query;
     const token = await getSession(user, pass);
     const schedule = await getSchedule(token);
@@ -28,24 +44,43 @@ router.get('/schedule', async (request) => {
 });
 
 router.get('/studentinfo', async (request) => {
+    const checkPass = await validateUserAndPass(request.query)
+    if(!checkPass) return new Response("Invalid username or password", { status: 400 })
     const { user, pass } = request.query;
     const token = await getSession(user, pass);
     const studentInfo = await getStudentInfo(token);
     return new Response(studentInfo);
 });
-/*
+
 router.get('/classes', async (request) => {
+    const checkPass = await validateUserAndPass(request.query)
+    if(!checkPass) return new Response("Invalid username or password", { status: 400 })
     const { user, pass } = request.query;
     const token = await getSession(user, pass);
     const classes = await getClasses(token);
     return new Response(classes);
-});*/
+});
 
 router.get('/gpa', async (request) => {
+    const checkPass = await validateUserAndPass(request.query)
+    if(!checkPass) return new Response("Invalid username or password", { status: 400 })
     const { user, pass } = request.query
     const token = await getSession(user, pass)
     const gpa = await getStudentGpa(token)
     return new Response(gpa)
+})
+
+router.get('/all', async (request) => {
+    const checkPass = await validateUserAndPass(request.query)
+    if(!checkPass) return new Response("Invalid username or password", { status: 400 })
+    const { user, pass } = request.query
+    const token = await getSession(user, pass)
+    return new Response({
+        schedule: await getSchedule(token),
+        studentInfo: await getStudentInfo(token),
+        classes: await getClasses(token),
+        gpa: await getStudentGpa(token)
+    })
 })
 
 router.all('*', () => new Response('Not Found.', { status: 404 }));
